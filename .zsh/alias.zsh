0bdf261b5b3f1acffd4d64a84bd083fb94b49189
muukii <m@muukii.me>
2017-04-10 11:35:47 +0900

    :evergreen_tree: Update

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -8,3 +8,23 @@ alias dat='docker attach `dl`'
 
 ## bear
 alias bear='~/dotfiles/scripts/bear.swift'
+
+alias ce='cd ../'
+alias ls='ls -F'
+alias la='ls -a'
+alias ll='ls -la'
+alias sl="ls"
+
+alias grep='grep --color=always'
+alias ngrep='grep -n --color=always'
+alias fgrep='fgrep --color=always'
+alias egrep='egrep --color=always'
+
+alias less='less -R'
+
+# alias for vim
+alias vi='vim -u $HOME/.virc'
+sw_vers=`which sw_vers 2>&1`
+if [[ $? == 0 ]]; then
+    alias vim='/Applications/MacVim.app/Contents/MacOS/Vim'
+fi
--- .zsh/git.zsh
+++ .zsh/git.zsh
@@ -9,3 +9,7 @@ function git_status() {
   local local_status=$(git status -s 2> /dev/null)
   echo $local_status
 }
+
+function parse_git_branch() {}
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/'
+}
